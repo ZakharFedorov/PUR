6c91269cc012fb6147d4499dff254edfdc2417c5
ZakharFedorov <zahar.fedo@mail.ru>
2026-05-10 17:06:35 +0200

    delete not needed things

--- src/main/java/com/example/recipe_manager/config/SecurityConfig.java
+++ src/main/java/com/example/recipe_manager/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/")
+                        .defaultSuccessUrl("/", true)
                         .permitAll()
                 )
                 .logout(logout -> logout
